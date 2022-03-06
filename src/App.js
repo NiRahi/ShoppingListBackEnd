47b0dc99a556172931d61d01ac66aff0dfae8d30
Nino <ninorahikkala@gmail.com>
2022-03-06 19:36:02 +0200

    FrontEnd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useEffect,useState} from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const URL = 'http://localhost:80/shoplist/'
@@ -11,53 +11,53 @@ function App() {
   const [quantity, setQuantity] = useState("")
   useEffect(() => {
     axios.get(URL + 'shoplist.php')
-    .then ((response) => {
-      setItems(response.data)
-      console.log(response.data)
+      .then((response) => {
+        setItems(response.data)
+        console.log(response.data)
 
-    }).catch(error => {
-      alert(error.response ? error.response.data.error : error);
-    })
-  
+      }).catch(error => {
+        alert(error.response ? error.response.data.error : error);
+      })
+
   }, [])
-  
+
   function save(e) {
     e.preventDefault();
-    console.log({amount:parseInt(quantity)})
+    console.log({ amount: parseInt(quantity) })
     const jsonItems = {
-      'description':item,
-      'amount':quantity
+      'description': item,
+      'amount': quantity
     };
-    
-    axios.post(URL + 'add.php',JSON.stringify(jsonItems),{
-      headers:{
-        'Content-Type' : 'application/json'
+
+    axios.post(URL + 'add.php', JSON.stringify(jsonItems), {
+      headers: {
+        'Content-Type': 'application/json'
       }
     })
 
 
-    .then ((response) => {
-      setItems(items => [...items,response.data]);
-      setItem("");
-      setQuantity("");
-    }).catch (error => {
-      alert(error.response.data.error)
-    }) 
+      .then((response) => {
+        setItems(items => [...items, response.data]);
+        setItem("");
+        setQuantity("");
+      }).catch(error => {
+        alert(error.response.data.error)
+      })
   }
 
   function remove(id) {
-    const json = JSON.stringify({id:id})
-    axios.post(URL+'delete.php',json,{
+    const json = JSON.stringify({ id: id })
+    axios.post(URL + 'delete.php', json, {
       headers: {
-        'Content-Type' : 'application/json'
+        'Content-Type': 'application/json'
       }
     })
-    .then((response) => {
-      const newListWithoutRemoved = items.filter((item, quantity) => item.id !== id);
-      setItems(newListWithoutRemoved);
-    }).catch(error=>{
-      alert(error.respone ? error.respone.data.error : error);
-    })
+      .then((response) => {
+        const newListWithoutRemoved = items.filter((item, quantity) => item.id !== id);
+        setItems(newListWithoutRemoved);
+      }).catch(error => {
+        alert(error.respone ? error.respone.data.error : error);
+      })
   }
   return (
     <div className='container'>
@@ -68,15 +68,15 @@ function App() {
         <label> Amount: </label>
         <input value={quantity} onChange={e => setQuantity(e.target.value)}></input>
         <button>Save</button>
-      </form>   
-    <ol>
-      {items?.map( item =>
-        <li key={item.id}>{item.description} {item.amount}&nbsp;
-        <a href='#' className='delete' onClick={() => remove(item.id)}>Delete</a>
-        
-        </li>
+      </form>
+      <ol>
+        {items?.map(item =>
+          <li key={item.id}>{item.description} {item.amount}&nbsp;
+            <a href='#' className='delete' onClick={() => remove(item.id)}>Delete</a>
+
+          </li>
         )}
-    </ol> 
+      </ol>
     </div>
   );
 }
